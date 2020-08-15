285f492e62c76cdc91566300475dab6403c5b570
Bakr Hamid <baker.pucit@gmail.com>
2020-08-16 01:35:47 +0500

    menu bar responsivness fixed

--- src/Components/Menu/menu.jsx
+++ src/Components/Menu/menu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Menu } from "antd";
+import { Menu, Row, Col } from "antd";
 
 const MenuItems = ({ items, mode, selectedKeys }) => {
   let style = "";
@@ -9,6 +9,7 @@ const MenuItems = ({ items, mode, selectedKeys }) => {
       return (style = {
         color: "black",
         borderBottom: "2px solid black",
+        textAlign: "center",
       });
     } else if (key === selectedKeys[0] && mode === "inline") {
       return (style = {
@@ -16,29 +17,61 @@ const MenuItems = ({ items, mode, selectedKeys }) => {
         borderRight: "2px solid black",
         backgroundColor: "lightgray",
       });
+    } else if (mode === "horizontal") {
+      return (style = {
+        color: "black",
+        borderBottom: "none",
+        textAlign: "center",
+        color: "gray",
+      });
     } else {
-      return (style = { color: "black", borderBottom: "none" });
+      return (style = {
+        color: "black",
+        borderBottom: "none",
+        color: "gray",
+      });
     }
   };
 
-  return (
-    <Menu
-      mode={mode}
-      selectedKeys={selectedKeys}
-      style={{ alignSelf: "center" }}
-    >
-      {items.map(({ label, key, icon }, i) => (
-        <Menu.Item
-          key={key || i}
-          icon={icon}
-          onClick={() => console.log("menu item clicked")}
-          style={getStyle(key)}
-        >
-          <span className="mt-2">{label}</span>
-        </Menu.Item>
-      ))}
-    </Menu>
-  );
+  //setting menu for horizontal mode and inline mode separatly
+  let menu = null;
+  if (mode === "horizontal") {
+    menu = (
+      <Menu mode={mode} selectedKeys={selectedKeys}>
+        <Row>
+          {items.map(({ label, key, icon }, i) => (
+            <Col key={key || i} xs={6} sm={6} md={6} lg={6} xl={6}>
+              <Menu.Item
+                key={key || i}
+                icon={icon}
+                onClick={() => console.log("menu item clicked")}
+                style={getStyle(key)}
+              >
+                <span className="mt-2">{label}</span>
+              </Menu.Item>
+            </Col>
+          ))}
+        </Row>
+      </Menu>
+    );
+  } else {
+    menu = (
+      <Menu mode={mode} selectedKeys={selectedKeys}>
+        {items.map(({ label, key, icon }, i) => (
+          <Menu.Item
+            key={key || i}
+            icon={icon}
+            onClick={() => console.log("menu item clicked")}
+            style={getStyle(key)}
+          >
+            <span className="mt-2">{label}</span>
+          </Menu.Item>
+        ))}
+      </Menu>
+    );
+  }
+
+  return <>{menu}</>;
 };
 
 export default MenuItems;
